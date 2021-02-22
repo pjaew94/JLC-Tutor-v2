b7f9b143ccd76dbb6cb11c6135433c1bf55e980f
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2021-02-22 13:49:07 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -14,14 +14,14 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/subjects", require("./routes/api/subjects"));
 
 // // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
   
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-//   }
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
   const PORT = process.env.PORT || 5000;
 
